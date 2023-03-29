86e8baecc7bc58f54c99d623056099c6ead7ced1
Pedro dos Santos Pinho <pedro.dossantos@inbrace.com>
2023-03-29 18:41:58 -0300

    Implemented Add and Complete todos to the web interface

--- web.py
+++ web.py
@@ -3,13 +3,28 @@ from modules import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    local_todo = st.session_state["new_todo"]
+    todos.append(local_todo + "\n")
+    functions.write_todos(todos)
+
+
 # Script is executed from top to bottom everytime the page is refreshed
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add new todo...")
+
+st.text_input(label="Add new todo", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo",
+              label_visibility='hidden')
